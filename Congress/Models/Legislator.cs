2b2227b7e79e49f9e1640ec0203516c0e6b6971f
Eric Mill <kprojection@gmail.com>
2010-08-19 18:11:14 -0400

    Reading in all the needed fields from a Legislator.

--- Congress/Models/Legislator.cs
+++ Congress/Models/Legislator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -9,8 +10,12 @@ using Newtonsoft.Json.Linq;
 namespace Congress.Models {
     public class Legislator {
 
-        public string bioguideId;
-        public string title, firstName, nickName, lastName, nameSuffix;
+        public string bioguideId; // unique ID
+        public string title, firstName, nickName, middleName, lastName, nameSuffix;
+        public string district, state, party, gender, senateClass;
+        public string website, phone, congressOffice;
+        public string youtubeUrl, twitterId;
+        public bool inOffice;
 
         public Legislator() {}
 
@@ -29,27 +34,56 @@ namespace Congress.Models {
         public delegate void LegislatorFoundEventHandler(Legislator legislator);
 
         public static Legislator oneFromJSON(string json) {
-            Legislator legislator = new Legislator();
-            
             JObject root = JObject.Parse(json);
-            root = (JObject) root["response"];
-            root = (JObject) root["legislator"];
-
-            legislator.bioguideId = (string) root["bioguide_id"];
-            legislator.title = (string) root["title"];
-            legislator.firstName = (string) root["firstname"];
-            legislator.lastName = (string) root["lastname"];
-            legislator.nickName = (string) root["nickname"];
-            legislator.nameSuffix = (string) root["name_suffix"];
-
-            return legislator;
+            JObject response = (JObject) root["response"];
+            JObject legislator = (JObject) response["legislator"];
+            return oneFromJObject(legislator);
         }
 
         public static Collection<Legislator> manyFromJSON(string json) {
+            JObject root = JObject.Parse(json);
+            JObject response = (JObject) root["response"];
+            JArray legislatorsRoot = (JArray) response["legislators"];
 
-            return null;
+            Collection<Legislator> legislators = new Collection<Legislator>();
+            foreach (JToken item in legislatorsRoot)
+                legislators.Add(oneFromJObject((JObject) item));
+
+            // sort by last name by default
+            legislators = (Collection<Legislator>) legislators.OrderBy(l => l.middleName);
+
+            return legislators;
         }
 
+        public static Legislator oneFromJObject(JObject root) {
+            Legislator legislator = new Legislator();
+
+            legislator.bioguideId = (string) root["bioguide_id"];
+
+            legislator.title = (string) root["title"];
+            legislator.firstName = (string) root["firstname"];
+            legislator.nickName = (string) root["nickname"];
+            legislator.middleName = (string) root["middlename"];
+            legislator.lastName = (string) root["lastname"];
+            legislator.nameSuffix = (string) root["name_suffix"];
+
+            legislator.district = (string) root["district"];
+            legislator.state = (string) root["state"];
+            legislator.party = (string) root["party"];
+            legislator.gender = (string) root["gender"];
+            legislator.senateClass = (string) root["senate_class"];
+
+            legislator.website = (string) root["website"];
+            legislator.phone = (string) root["phone"];
+            legislator.congressOffice = (string) root["congress_office"];
+            
+            legislator.youtubeUrl = (string) root["youtube_url"];
+            legislator.twitterId = (string) root["twitter_id"];
+
+            legislator.inOffice = (bool) root["in_office"];
+            
+            return legislator;
+        }
 
 
         public class Sunlight {
