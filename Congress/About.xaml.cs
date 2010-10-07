8e4f5d0abee9764fd51be129d28d8b91c6a53252
Eric Mill <kprojection@gmail.com>
2010-10-07 11:30:12 -0400

    Added tech support info to About page

--- Congress/About.xaml.cs
+++ Congress/About.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
 
 namespace Congress {
     public partial class About : PhoneApplicationPage {
@@ -18,8 +19,11 @@ namespace Congress {
             InitializeComponent();
         }
 
-        private void returnHome(object sender, EventArgs e) {
-            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        private void launchEmail(object sender, MouseButtonEventArgs e) {
+            EmailComposeTask task = new EmailComposeTask();
+            task.Subject = Congress.Strings.EmailSubject;
+            task.To = Congress.Strings.EmailTo;
+            task.Show();
         }
     }
 }
